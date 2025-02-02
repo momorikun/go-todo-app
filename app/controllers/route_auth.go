f6a5221190258593825f92e40f3ff77c47f7b768
momorikun <119ryota@gmail.com>
2025-02-03 07:48:30 +0900

    second commit

---
+++ app/controllers/route_auth.go
@@ -0,0 +1,64 @@
+package controllers
+
+import (
+	"log"
+	"net/http"
+	"todo_app/app/models"
+)
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+
+		http.Redirect(w, r, "/", http.StatusFound)
+	}
+}
+
+func login(w http.ResponseWriter, r *http.Request) {
+	generateHTML(w, nil, "layout", "public_navbar", "login")
+}
+
+func authenticate(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+	user, err := models.GetUserByEmail(r.PostFormValue("email"))
+	if err != nil {
+		log.Println(err)
+		http.Redirect(w, r, "/login", http.StatusFound)
+	}
+	log.Println("user.PassWord: " + user.PassWord)
+	log.Println("models.Encrypt(r.PostFormValue('password')): " + models.Encrypt(r.PostFormValue("password")))
+	log.Println(r.PostFormValue("password"))
+	if user.PassWord == models.Encrypt(r.PostFormValue("password")) {
+		session, err := user.CreateSession()
+		if err != nil {
+			log.Println(err)
+		}
+
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    session.UUID,
+			HttpOnly: true,
+		}
+		http.SetCookie(w, &cookie)
+		http.Redirect(w, r, "/", http.StatusFound)
+	} else {
+		http.Redirect(w, r, "/login", http.StatusFound)
+	}
+}
